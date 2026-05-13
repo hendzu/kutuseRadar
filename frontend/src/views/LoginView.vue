1cd4b78695b00e2df40458f73a4f3e9c57d82083
Hendzu <hendrikrasmus@hotmail.com>
2026-05-13 13:13:25 +0300

    created login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -2,20 +2,72 @@
   <div class="row justify-content-center mt-5">
     <h1 class="text-center">Logi sisse</h1>
     <div class="col col-3 mb-4">
-      <input type="text" class="form-control" id="inputUsername" placeholder="Kasutajanimi" />
+      <input
+        v-model="username"
+        type="text"
+        class="form-control"
+        id="inputUsername"
+        placeholder="Kasutajanimi"
+      />
     </div>
   </div>
-  <div class="row justify-content-center">
+  <div class="row justify-content-center mb-3">
     <div class="col col-3">
-      <input type="text" class="form-control" id="inputPassword" placeholder="Parool" />
+      <input
+        v-model="password"
+        type="text"
+        class="form-control"
+        id="inputPassword"
+        placeholder="Parool"
+      />
     </div>
   </div>
-  <div class="row justify-content-center mt-3">
+  <div class="row justify-content-center mb-3">
     <div class="col col-3">
-      <button class="btn btn-primary w-100">Logi sisse</button>
+      <button @click="login" class="btn btn-primary w-100">Logi sisse</button>
     </div>
   </div>
 </template>
 
-<style scoped></style>
-<script setup></script>
+<script>
+import LoginService from '@/api-services/LoginService.js'
+
+export default {
+  name: 'LoginView',
+  data() {
+    return {
+      username: '',
+      password: '',
+      errorMessage: '',
+      loginResponse: {
+        userId: 0,
+        roleName: '',
+      },
+    }
+  },
+  methods: {
+    allFormFieldsAreCorrect() {
+      return this.username && this.password
+    },
+    login() {
+      //   this.startSpinner()
+      //   this.resetErrorMessage()
+      if (this.allFormFieldsAreCorrect()) {
+        LoginService.sendGetLoginRequest(this.username, this.password)
+          .then((response) => this.handleLoginResponse(response))
+          .catch((error) => this.handleLoginError(error))
+          .finally()
+      } else {
+        this.errorMessage = 'Täida kõik väljad'
+      }
+    },
+    handleLoginResponse: (response) => {
+      this.loginResponse = response.data()
+      localStorage.setItem('userId', this.loginResponse.userId)
+      localStorage.setItem('roleName', this.loginResponse.roleName)
+      this.$emit('event-user-logged-in')
+      return undefined
+    },
+  },
+}
+</script>
